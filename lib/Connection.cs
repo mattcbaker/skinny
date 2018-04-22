754350383e3d192781e046149e798a84b9686081
Matt Baker <baker.mattc@gmail.com>
2018-04-21 22:53:15 -0400

    return number of rows altered when command alters rows

--- lib/Connection.cs
+++ lib/Connection.cs
@@ -1,15 +1,31 @@
-﻿namespace Skinny
-{
-    public class Connection
-    {
-        public Connection(string connectionString)
-        {
-            
-        }
+﻿using Npgsql;
 
-        public int Command(string command)
-        {
-            return -1;
-        }
+namespace Skinny
+{
+  public class Connection
+  {
+    readonly string connectionString;
+
+    public Connection(string connectionString)
+    {
+      this.connectionString = connectionString;
     }
+
+    public int Command(string command)
+    {
+      var postgresConnection = OpenPostgresConnection();
+
+      var postgresCommand = postgresConnection.CreateCommand();
+      postgresCommand.CommandText = command;
+
+      return postgresCommand.ExecuteNonQuery();
+    }
+
+    NpgsqlConnection OpenPostgresConnection()
+    {
+      var connection = new NpgsqlConnection(connectionString);
+      connection.Open();
+      return connection;
+    }
+  }
 }
--- tests/ConnectionTests.cs
+++ tests/ConnectionTests.cs
@@ -1,25 +1,52 @@
 using System;
 using Xunit;
 
+// Ref https://xunit.github.io/docs/running-tests-in-parallel
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+
 namespace Skinny
 {
-    public class when_executing_a_command_that_does_not_alter_rows : IDisposable
+  public class when_executing_a_command_that_does_not_alter_rows
+  {
+    public when_executing_a_command_that_does_not_alter_rows()
     {
-        public when_executing_a_command_that_does_not_alter_rows()
-        {
-            var connection = new Connection(Settings.ConnectionString);
-            var command = "CREATE TABLE skinny_testing";
+      var connection = new Connection(Settings.ConnectionString);
 
-            actual = connection.Command(command);
-        }
-        public void Dispose()
-        {
-            // cleanup table using ngpsql
-        }
+      var dropTableCommand = "DROP TABLE IF EXISTS skinny_testing";
+      connection.Command(dropTableCommand);
 
-        [Fact]
-        public void should_return_negative_one() => Assert.Equal(-1, actual);
+      var command = "CREATE TABLE skinny_testing (title varchar(100))";
 
-        static int actual;
+      actual = connection.Command(command);
     }
+
+    [Fact]
+    public void should_return_negative_one() => Assert.Equal(-1, actual);
+
+    static int actual;
+  }
+
+  public class when_executing_a_command_that_does_alter_rows
+  {
+    public when_executing_a_command_that_does_alter_rows()
+    {
+      var connection = new Connection(Settings.ConnectionString);
+
+      var dropTableCommand = "DROP TABLE IF EXISTS skinny_testing";
+      connection.Command(dropTableCommand);
+
+      var createTableCommand = "CREATE TABLE skinny_testing (title varchar(100))";
+
+      connection.Command(createTableCommand);
+
+      var insertCommand = "INSERT INTO skinny_testing (title) VALUES ('some testing')";
+
+      actual = connection.Command(insertCommand);
+    }
+
+    [Fact]
+    public void should_return_negative_one() => Assert.Equal(1, actual);
+
+    static int actual;
+  }
 }
